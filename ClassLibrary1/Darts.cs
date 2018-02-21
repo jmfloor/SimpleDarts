c69e8e1948218bfbc83d88a9edea5cbeff28e3cd
Johannes <33619163+jmfloor@users.noreply.github.com>
2018-02-21 09:53:58 +0200

    build Dart class that will determine score for each throw

---
+++ ClassLibrary1/Darts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Darts
+{
+    public class Dart
+    {
+        Random random = new Random();
+
+        public int Score { get; set; }
+        public int Band { get; set; }
+
+
+        public void Throw()
+        {
+            Score = this.random.Next(20);
+            
+            if (random.Next(100) <= 5)
+            {
+                this.Band = random.Next(2);
+            }
+            else this.Band = 0;
+            
+            //comment
+        }
+
+    }
+}
