fd8c15e02fa65a925b5ae38048659afd0ae84087
Johannes <33619163+jmfloor@users.noreply.github.com>
2018-02-23 10:09:55 +0200

    completed game logic and display results

--- ClassLibrary1/Darts.cs
+++ ClassLibrary1/Darts.cs
@@ -11,14 +11,17 @@ namespace Darts
 
         public int Score { get; set; }
         public int Band { get; set; }
-
-
+        
         public void Throw()
         {
             Score = this.random.Next(20);
             
             if (random.Next(100) <= 5)
             {
+                // 0 = no band
+                // 1 = outerband
+                // 2 = innerband
+
                 this.Band = random.Next(2);
             }
             else this.Band = 0;
---
+++ SimpleDarts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleDarts
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+
+        public void IncreaseScore(int score)
+        {
+            this.Score += score;
+        }
+
+    }
+
+    
+}
